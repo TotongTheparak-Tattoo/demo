2e97a404fb2da34c0294985720a208466cf3610d
TOTONGTHEPARAK <138663933+TOTONGTHEPARAK@users.noreply.github.com>
2025-11-20 13:48:09 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 // require('dotenv').config({ path: '.env.production' });
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4022;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
   // Initial database
